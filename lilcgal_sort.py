8a4eb5bed4fff154cbec04bebb3436bf5cfb8abc
Jose M Perez <jose.m.perez.ramos+git@gmail.com>
2017-09-24 22:16:31 +0200

    Added some sorting functions

--- lilcgal_sort.py
+++ lilcgal_sort.py
@@ -6,6 +6,7 @@ from math import *
 def sort_x(L):
     return sorted(L)
 
+# Vector dir
 
 def sort_vectorDir_dot(L, Vector):
     X = Vector[0]
@@ -40,16 +41,17 @@ def sort_vectorDir_dot_customfun(L, Vector):
             return 1;
         elif R <0:
             return -1;
-        else:
-            R = PointA[0] - PointB[0]
-            if R == 0:
-                return PointA[1] - PointB[1]
-            else:
-                return R
 
-    return sorted(L, cmp )
+        R = PointA[0] - PointB[0]
+        if R == 0:
+            return PointA[1] - PointB[1]
+        else:
+            return R
+
+    return sorted(L, cmp)
 
 def sort_vectorDir_dot_customlambda(L, Vector):
+    print("Do not use this function: sort_vectorDir_dot_customlambda")
     # Do not use, it's wrong (int(-0.5) = 0) and does not account for ties
     X = Vector[0]
     Y = Vector[1]
@@ -71,4 +73,106 @@ def sort_vectorDir(L, V):
         return sort_vectorDir_dot_tuple(L, V)
     else:
         return sort_vectorDir_dot_customfun(L, V)
+
+# Angle dir
+
+def sort_angleFromPoint_atan2(L, Point):
+    X = Point[0]
+    Y = Point[1]
+
+    L_wAngle = list(map(lambda Point: (atan2(Point[1] - Y, Point[0] - X) % (2*pi), Point), L))
+    L_sorted = sorted(L_wAngle)
+    return list(map(lambda AngleTuple: AngleTuple[1], L_sorted))
+
+def sort_angleFromPoint_sarea(L, PointO):
+    def cmp(PointA, PointB):
+        R = sarea(PointO, PointA, PointB)
+        if R >0:
+            return 1;
+        elif R <0:
+            return -1;
+
+        R = PointA[0] - PointB[0]
+        if R == 0:
+            return PointA[1] - PointB[1]
+        else:
+            return R
+
+    Lup   = filter(lambda Point: Point[1] >= PointO[1], L)
+    Ldown = filter(lambda Point: Point[1] <  PointO[1], L)
+
+    return sorted(Lup, cmp) + sorted(Ldown, cmp)
+
+def sort_angleFromPoint_sarea2(L, PointO):
+    def cmp(PointA, PointB):
+        RelYA = PointA[1] - PointO[1]
+        RelYB = PointB[1] - PointO[1]
+        if RelYA * RelYB < 0: #different semis
+            if RelYA > 0:
+                return -1
+            else:
+                return 1
+        elif RelYA * RelYB == 0: # At least one is 0
+            if RelYA < 0:
+                return 1
+            elif RelYB < 0:
+                return -1
+
+        R = sarea(PointO, PointA, PointB)
+        if R >0:
+            return 1;
+        elif R <0:
+            return -1;
+
+        R = PointA[0] - PointB[0]
+        if R == 0:
+            return PointA[1] - PointB[1]
+        else:
+            return R
+
+    return sorted(L, cmp)
+
+def sort_angleFromPoint(L, P):
+    return sort_angleFromPoint_atan2(L, P) # seems to be always faster
+
+
+# Slope sort
+def sort_slope_tuple(L):
+    inf = float('inf')
+    L_wSlope = list(map(lambda Line: ((Line[1][1] - Line[0][1]) / float(Line[1][0] - Line[0][0]) if (Line[1][0] - Line[0][0]) else inf, Line), L))
+    L_sorted = sorted(L_wSlope)
+    return list(map(lambda Tuple: Tuple[1], L_sorted))
+
+def sort_slope_cmp(L):
+    def cmp(LineA, LineB):
+        VectorA = diff(LineA[1], LineA[0])
+        VectorB = diff(LineB[1], LineB[0])
+        R = VectorB[0] * VectorA[1] - VectorA[0] * VectorB[1]
+        if R >0:
+            return 1;
+        elif R <0:
+            return -1;
+
+        if LineA < LineB:
+            return -1
+        elif LineA > LineB:
+            return 1
+        else:
+            return 0
+
+    return sorted(L, cmp)
+
+def sort_slope(L):
+    return sort_slope_tuple(L)
+
+# Proyection sort
+def proyection_sort(L, Line):
+    return sort_vectorDir(L, diff(Line[1], Line[0]))
+
+# Distance sort
+def distance_sort(L, Line):
+    A = Line[0]
+    B = Line[1]
+    L_sorted = sorted(list(map(lambda Point: (abs(sarea(A, B, Point)), Point), L)))
+    return list(map(lambda Tuple: Tuple[1], L_sorted))
 
--- test_sort.py
+++ test_sort.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/sage -python
+
+##!/usr/bin/env python2
 
 from lilcgal_sort import *
+from sage.all import *
 from random import randint, random
+import time
 
 def main():
     L = []
@@ -9,18 +13,63 @@ def main():
         L.append([randint(0,10), randint(0,10)])
 
     print(L)
-    print(sort_x(L))
+    G = point(L, color = 'red', size = 10)
+    XSort = sort_x(L)
+    print(XSort)
+    # G += line(XSort)
 
     print
     print(sort_vectorDir_dot(L, [1,0]))
     print(sort_vectorDir_dot2(L, [1,0]))
     print(sort_vectorDir_dot_customfun(L, [1,0]))
-    print(sort_vectorDir_dot_customlambda(L, [1,0]))
+    # print(sort_vectorDir_dot_customlambda(L, [1,0]))
     print(sort_vectorDir_matrixTurn(L, [1,0]))
 
     print
-    print(sort_vectorDir(L, [1, 0]))
-    print(sort_vectorDir(L, [0, 1]))
+    VectorSort = sort_vectorDir(L, [1, 0])
+    print(VectorSort)
+    # G += line(VectorSort)
+    VectorSort = sort_vectorDir(L, [0, 1])
+    print(VectorSort)
+    # G += line(VectorSort)
+
+    print
+    AngleSort = sort_angleFromPoint_atan2(L, [5,5])
+    print(AngleSort)
+    # G += line(AngleSort)
+    AngleSort = sort_angleFromPoint_sarea(L, [5,5])
+    print(AngleSort)
+    # G += line(AngleSort)
+    print(sort_angleFromPoint_atan2([[4,4],[3,3],[5,5],[6,6],[1,1],[2,2],[7,7],[0,8]], [5,5]))
+    print(sort_angleFromPoint_sarea([[4,4],[3,3],[5,5],[6,6],[1,1],[2,2],[7,7],[0,8]], [5,5]))
+    print(sort_angleFromPoint_sarea2([[4,4],[3,3],[5,5],[6,6],[1,1],[2,2],[7,7],[0,8]], [5,5]))
+
+    print
+    L.append([0,10])
+    print(sort_angleFromPoint_atan2(L, [0,0]))
+    LL = [[[0,0],X] for X in L]
+    SlopeSort = sort_slope_tuple(LL)
+    print(list(map(lambda Tuple: Tuple[1], SlopeSort)))
+    SlopeSort = sort_slope_cmp(LL)
+    print(list(map(lambda Tuple: Tuple[1], SlopeSort)))
+
+    print
+    ProyectionLine = [[0,6],[5,0]]
+    # G += line(ProyectionLine, color= 'red')
+    ProyectionSort = proyection_sort(L, ProyectionLine)
+    # G += line(ProyectionSort)
+    print(ProyectionSort)
+
+    print
+    DistanceLine = [[0,8],[8,0]]
+    G += line(DistanceLine, color= 'red')
+    DistanceSort = distance_sort(L, DistanceLine)
+    G += line(DistanceSort)
+    print(DistanceSort)
+
+    save(G,'/tmp/dom.png',aspect_ratio=True)
+    os.system('feh /tmp/dom.png')
+
 
 if __name__ == "__main__":
     main()
